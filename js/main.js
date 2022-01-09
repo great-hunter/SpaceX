ac6c5c565758aaf1f37488d86e16556ab3a53b1c
great-hunter <75899280+great-hunter@users.noreply.github.com>
2022-01-09 03:52:18 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var instance = new vidbg(
   ".video",
   {
     mp4: "video/world.mp4", // URL or relative path to MP4 video
-    webm: "video/world.webm", // URL or relative path to webm video
+   // webm: "video/world.webm", // URL or relative path to webm video
     poster: "video/poster.jpg", // URL or relative path to fallback image
     overlay: false, // Boolean to display the overlay or not
   },
@@ -11,3 +11,7 @@ var instance = new vidbg(
   }
 );
 var rellax = new Rellax('.rocket');
+if (document.body.clientWidth < 576) {
+  rellax.destroy();
+}
+AOS.init();
